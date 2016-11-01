4f9a0c0d1abb61fc8cdbc27d67776207a665e44d
Cormac De Faoite <cwhite788@gmail.com>
2016-11-01 20:41:26 +0000

    Updated Main.cpp

--- TextBasedEngine/TextBasedEngine/Main.cpp
+++ TextBasedEngine/TextBasedEngine/Main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+#include "Object.h"
+
+#include "Character.h"
+#include "PlayerCharacter.h"
+#include "EnemyCharacter.h"
+#include "InteractableCharacter.h"
+
+#include "Item.h"
+#include "DynamicItem.h"
+#include "Weapon.h"
+#include "Key.h"
+#include "Consumable.h"
+
+#include "Scenary.h"
+#include "Room.h"
+
 using namespace std;
 
 int main()
 {
+
 	system("pause");
 	return 0;
 }
